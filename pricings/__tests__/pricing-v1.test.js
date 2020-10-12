c202bd6ecc6dd9175ebaae1cdf5f77f83753c58b
Ludo171 <lddsctx+github@gmail.com>
2020-10-12 03:58:08 +0200

    Avoid time zone dependency in pricing

--- pricings/__tests__/pricing-v1.test.js
+++ pricings/__tests__/pricing-v1.test.js
@@ -4,16 +4,16 @@ describe('Test Suite - Pricing V1', () => {
   it('Correct price for Non-Busy time trips and Non-Night-Shift trips', () => {
     const testEntries = [
       {
-        distance: 10, duration: 3600, startTime: '2020-10-12T04:30:00.000Z', // ~> 6:30 AM -> 7:30 AM
+        distance: 10, duration: 3600, startTime: '2020-10-12T06:30:00.000Z', // ~> 6:30 AM -> 7:30 AM
       },
       {
-        distance: 5, duration: 1800, startTime: '2020-01-15T12:08:12.000Z', // ~> 2:08 PM -> 2:38 PM
+        distance: 5, duration: 1800, startTime: '2020-01-15T14:08:12.000Z', // ~> 2:08 PM -> 2:38 PM
       },
       {
-        distance: 40, duration: 240, startTime: '2018-12-15T13:55:00.000Z', // ~> 3:55 PM -> 3:59 PM
+        distance: 40, duration: 240, startTime: '2018-12-15T15:55:00.000Z', // ~> 3:55 PM -> 3:59 PM
       },
       {
-        distance: 20, duration: 1800, startTime: '2020-10-15T17:02:00.000Z', // ~> 7:02 PM -> 7:32 PM
+        distance: 20, duration: 1800, startTime: '2020-10-15T19:02:00.000Z', // ~> 7:02 PM -> 7:32 PM
       },
     ];
 
@@ -26,16 +26,16 @@ describe('Test Suite - Pricing V1', () => {
   it('Correct price for busy-time trips', () => {
     const testEntries = [
       {
-        distance: 10, duration: 300, startTime: '2020-10-12T14:30:00.000Z', // ~> 4:30 PM -> 4:35 PM
+        distance: 10, duration: 300, startTime: '2020-10-12T16:30:00.000Z', // ~> 4:30 PM -> 4:35 PM
       },
       {
-        distance: 5, duration: 7200, startTime: '2020-10-02T13:55:12.000Z', // ~> 3:55 PM -> 5:55 PM
+        distance: 5, duration: 7200, startTime: '2020-10-02T15:55:12.000Z', // ~> 3:55 PM -> 5:55 PM
       },
       {
-        distance: 40, duration: 16200, startTime: '2020-09-15T13:00:00.000Z', // ~> 3:00 PM -> 7:30 PM
+        distance: 40, duration: 16200, startTime: '2020-09-15T15:00:00.000Z', // ~> 3:00 PM -> 7:30 PM
       },
       {
-        distance: 20, duration: 3600, startTime: '2020-10-16T16:35:00.000Z', // ~> 6:35 PM -> 7:35 PM
+        distance: 20, duration: 3600, startTime: '2020-10-16T18:35:00.000Z', // ~> 6:35 PM -> 7:35 PM
       },
     ];
 
@@ -48,19 +48,19 @@ describe('Test Suite - Pricing V1', () => {
   it('Correct price for night-shift trips', () => {
     const testEntries = [
       {
-        distance: 10, duration: 300, startTime: '2020-10-12T18:30:00.000Z', // ~> 8:30 PM -> 8:35 PM
+        distance: 10, duration: 300, startTime: '2020-10-12T20:30:00.000Z', // ~> 8:30 PM -> 8:35 PM
       },
       {
-        distance: 5, duration: 7200, startTime: '2020-10-15T17:55:00.000Z', // ~> 7:55 PM -> 9:55 PM
+        distance: 5, duration: 7200, startTime: '2020-10-15T19:55:00.000Z', // ~> 7:55 PM -> 9:55 PM
       },
       {
-        distance: 40, duration: 43200, startTime: '2020-10-15T17:01:00.000Z', // ~> 7:01 PM -> 7:01 AM
+        distance: 40, duration: 43200, startTime: '2020-10-15T19:01:00.000Z', // ~> 7:01 PM -> 7:01 AM
       },
       {
-        distance: 20, duration: 3600, startTime: '2020-10-16T03:35:00.000Z', // ~> 5:35 AM -> 6:35 PM
+        distance: 20, duration: 3600, startTime: '2020-10-16T05:35:00.000Z', // ~> 5:35 AM -> 6:35 PM
       },
       {
-        distance: 1, duration: 7200, startTime: '2020-10-16T21:35:00.000Z', // ~> 11:35 PM -> 1:35 PM
+        distance: 1, duration: 7200, startTime: '2020-10-16T23:35:00.000Z', // ~> 11:35 PM -> 1:35 PM
       },
     ];
 
@@ -73,13 +73,13 @@ describe('Test Suite - Pricing V1', () => {
   it('Correct price for Busy + Night-shift trips', () => {
     const testEntries = [
       {
-        distance: 10, duration: 7200, startTime: '2020-10-12T16:30:00.000Z', // ~> 6:30 PM -> 8:30 PM
+        distance: 10, duration: 7200, startTime: '2020-10-12T18:30:00.000Z', // ~> 6:30 PM -> 8:30 PM
       },
       {
-        distance: 5, duration: 21600, startTime: '2020-01-15T16:55:00.000Z', // ~> 6:55 PM -> 12:55 AM
+        distance: 5, duration: 21600, startTime: '2020-01-15T18:55:00.000Z', // ~> 6:55 PM -> 12:55 AM
       },
       {
-        distance: 40, duration: 32400, startTime: '2020-10-15T13:00:00.000Z', // ~> 3:00 PM -> 12:00 PM
+        distance: 40, duration: 32400, startTime: '2020-10-15T15:00:00.000Z', // ~> 3:00 PM -> 12:00 PM
       },
     ];
 
--- pricings/pricing-v1.js
+++ pricings/pricing-v1.js
@@ -20,12 +20,11 @@ const pricingV1 = (body) => {
   // 2) + .50 EUR per 1/5 th of a mile (~ +2.5€/mile)
   price.amount += 2.5 * body.distance;
 
+  const startHour = (new Date(body.startTime.substr(0, 19))).getHours();
+  const endHour = (new Date(Date.parse(body.startTime.substr(0, 19)) + body.duration * 1000)).getHours();
 
-  const startHour = (new Date(body.startTime)).getHours();
-  const endHour = (new Date(Date.parse(body.startTime) + body.duration * 1000)).getHours();
-
-  const temp = new Date(body.startTime);
-  const temp2 = new Date(Date.parse(body.startTime) + body.duration * 1000);
+  const temp = new Date(body.startTime.substr(0, 19));
+  const temp2 = new Date(Date.parse(body.startTime.substr(0, 19)) + body.duration * 1000);
   const isRoundClockTrip = temp2 - temp > 24 * 60 * 60 * 1000;
 
   // 3) + .50 additional EUR between 8PM and 6AM
